e93520c25db2a483ecd70120df99cd78f6afaa09
Suganth A <asuganth2003@gmail.com>
2024-06-17 15:24:33 +0530

    first commit

---
+++ src/_auth/AuthLayout.tsx
@@ -0,0 +1,27 @@
+import { Outlet, Navigate  } from "react-router-dom"
+
+const AuthLayout = () => {
+  const isAuthenticate=false;
+  return (
+    <>
+    {isAuthenticate ? (
+      <Navigate to ="/"/>
+    ):
+    (
+      <>
+      <section className="flex flex-1 justify-center items-center flex-col py-10">
+        <Outlet/> 
+      </section>
+      <img
+        src="/images/img1.jpg"
+        alt="logo"
+        className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
+        style={{ transform: "skew(-10deg)" }}
+/>
+</>
+    )}
+    </>
+  )
+}
+
+export default AuthLayout
---
+++ src/components/shared/Loader.tsx
@@ -0,0 +1,7 @@
+const Loader = () => {
+  return (
+    <div className="flex-center w-full">Loader</div>
+  )
+}
+
+export default Loader
