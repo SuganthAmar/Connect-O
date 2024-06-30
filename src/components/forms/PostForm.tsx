64f8fcada54bd3c281dd28d1c8ee22a7737afe0c
Suganth A <asuganth2003@gmail.com>
2024-06-30 18:17:59 +0530

    Post Creation

--- src/components/forms/PostForm.tsx
+++ src/components/forms/PostForm.tsx
@@ -2,19 +2,16 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
 import { Button } from "@/components/ui/button"
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Textarea } from "../ui/textarea"
 import FileUploader from "../shared/FileUploader"
 import { PostValidation } from "@/lib/validation"
 import { Models } from "appwrite"
+import { useCreatePost } from "@/lib/react-query/queriesAndMutations"
+import { useUserContext } from "@/context/AuthContext"
+import { useToast } from "../ui/use-toast"
+import { useNavigate } from "react-router-dom"
  
 type PostFormProps = {
     post? : Models.Document;
@@ -23,6 +20,9 @@ type PostFormProps = {
 
 const PostForm = ({post} : PostFormProps) => {
     const { mutateAsync: createPost, isPending: isLoadingCreate} = useCreatePost ();
+    const { user } = useUserContext();
+    const { toast } = useToast();
+    const navigate = useNavigate();
 
     const form = useForm<z.infer<typeof PostValidation>>({
         resolver: zodResolver(PostValidation),
@@ -36,8 +36,17 @@ const PostForm = ({post} : PostFormProps) => {
       })
      
       // 2. Define a submit handler.
-      function onSubmit(values: z.infer<typeof PostValidation>) {
-        
+      async function onSubmit(values: z.infer<typeof PostValidation>) {
+        const newPost = await createPost({
+          ...values,
+          userId: user.id,
+        })
+        if(!newPost){
+          toast({
+            title: "Error creating post",
+          })
+        }
+        navigate('/');
       }
   return (
     <Form {...form}>
